26a81a0464594d24ef3e51236cef90df22671adc
Nguyen Hoai An <anmom8910@gmail.com>
2025-06-01 23:06:23 +0700

    Lưu thay đổi tạm thời trước khi checkout

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import BlogPage from "./components/BlogPage";
+import LoginPage from "./components/LoginPage";
+import { UserContext } from "./UserContext";
+
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/gender-health-care/signingoogle", {
+        withCredentials: true, // để gửi cookie nếu có
+      })
+      .then((res) => {
+        setUser(res.data.user);
+      })
+      .catch(() => {
+        setUser(null);
+      });
+  }, []);
+
+  return (
+    <UserContext.Provider value={{user}}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
