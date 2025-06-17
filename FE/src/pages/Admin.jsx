f9a98f4ac604b6479a6ab423269f7f5a85dac3b2
khoinm04 <khoiminhnguyen2k4@gmail.com>
2025-06-17 13:27:49 +0700

    changes in main put into oldMain

--- FE/src/pages/Admin.jsx
+++
@@ -1,7 +0,0 @@
-import AdminDashboard from '../../components/AdminDashboard'; // Điều chỉnh đường dẫn nếu cần
-
-const AdminDashboardPage = () => {
-  return <AdminDashboard />;
-};
-
-export default AdminDashboardPage;
